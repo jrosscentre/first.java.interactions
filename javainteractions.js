8f25868697ebe8634843bd67056e7c8b7a7bcb0c
Jerry <j.ross@centre.edu>
2019-09-19 03:43:41 -0400

    done

---
+++ javainteractions.js
@@ -0,0 +1,62 @@
+var nastymessage = function()
+
+{
+    var node = document.createElement("h1");
+        node.innerText = "you would";
+    
+    var body = document.querySelector("body");
+        body.appendChild(node);
+    var button = document.querySelector("body");
+        node.setAttribute("onclick");
+    var button = document.querySelector("#red1button");
+    
+    
+    
+}
+
+var nextmessage = function()
+{
+     var node = document.createElement("h1");
+        node.innerText = "now choose another";
+    var body = document.querySelector("body");
+        body.appendChild(node);
+    var button = document.querySelector("body");
+        node.setAttribute("onclick");
+    var button = document.querySelector("#red2button");
+}
+
+var congratulations = function()
+{
+     var node = document.createElement("h1").style.color = "red";
+        node.innerText = "CONGRATULATIONS!";
+    
+    var body = document.querySelector("body");
+        body.appendChild(node);
+    var button = document.querySelector("body");
+        node.setAttribute("onclick");
+    var button = document.querySelector("#red3button");
+}
+
+var idiot = function()
+{
+     var node = document.createElement("h1");
+        node.innerText = "you really thought it would be the last one...pardon my french but youre an idiot"
+    var body = document.querySelector("body");
+        body.appendChild(node);
+    var button = document.querySelector("body");
+        node.setAttribute("onclick");
+    var button = document.querySelector("#red4button");
+}
+
+
+var turnboxred = function()
+{
+    var color = ["brown"]
+    var body = document.querySelector("div.redbox1")
+    
+}
+
+var turnboxbrown = function()
+{
+    
+}
